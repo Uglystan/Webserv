05e44d6d731952d7c30030b226e1403195aec1dd
Uglystan <lucasgirault@orange.fr>
2023-10-11 15:04:06 +0200

    Chunk patch

--- srcs/server/recvMultipart.cpp
+++ srcs/server/recvMultipart.cpp
@@ -28,11 +28,46 @@ void	recvBase(t_server &data, int &clientSocket)
 	data.req[clientSocket].message.erase();
 }
 
+int	is_hexa(char c)
+{
+	if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f'))
+		return (0);
+	return (1);
+}
+
+int	next_line(t_server &data, int i, int clientSocket)
+{
+	while(data.req[clientSocket].message[i] != '\n')
+		i++;
+	return (i + 1);
+}
+
+int	chunkTest(t_server &data, int &clientSocket)
+{
+	int	j;
+	int i = 0;
+	while (i <= data.req[clientSocket].bytes)
+	{
+		j = i;
+		while (is_hexa(data.req[clientSocket].message[j]) == 0)
+		{
+			j++;
+			if (data.req[clientSocket].message[j] == '\r')
+			{
+				data.req[clientSocket].message.erase(i, j + 1);
+				break;
+			}
+		}
+		i = next_line(data, i, clientSocket);
+	}
+}
+
 void	recvChunk(t_server &data, int &clientSocket)
 {
-	if (data.req[clientSocket].message.find("0\r\n\r\n") != std::string::npos)
+	if (data.req[clientSocket].message.find("0\r\n\r\n") != std::string::npos)//message contient encore les tailles
 	{
 		//std::cout << "Message recu en chunk : " << data.req[clientSocket].message << std::endl;
+		chunkTest(data, clientSocket);
 		Response reponse(data.req[clientSocket].message, findGoodServ(data.req[clientSocket].message, data));
 		std::string response = reponse.statik_or_dynamik();
 		if (send(clientSocket, response.data(), response.size(), 0) == -1)
