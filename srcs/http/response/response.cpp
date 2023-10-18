4b31734a2b6e1894defe6b853c66533eaf7e9099
alex <abourdon@paul-f4Br4s1.clusters.42paris.fr>
2023-10-18 14:09:15 +0200

    push

--- srcs/http/response/response.cpp
+++ srcs/http/response/response.cpp
@@ -110,18 +110,17 @@ std::string	Response::statik_or_dynamik(void)
 		}
 		else
 			statik_response();
-		std::cout << "Message envoyee : \n" << _response << std::endl;
+		//std::cout << "Message envoyee : \n" << _response << std::endl;
 		return (_response);
 	}
 	catch (const std::exception& e)
 	{
 		_response.clear();
-		std::cout << "CODE : " << _code << std::endl;
 		body_error_page();
 		create_header();
 		std::cerr << e.what() << std::endl;
 		_response = _header + _body;
-		std::cout << "Message envoyee : \n" << _response << std::endl;
+		//std::cout << "Message envoyee : \n" << _response << std::endl;
 		return (_response);
 	}
 }
